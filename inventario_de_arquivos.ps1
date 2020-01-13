1d3996b98aaf2c92030e54049e6be83770537355
Fernando Gomes <feromes@gmail.com>
2020-01-13 12:02:58 -0300

    Versão em produção com PowerShell

---
+++ inventario_de_arquivos.ps1
@@ -0,0 +1,21 @@
+$campos = "FullName,Name,Extension,Length,CreationTime,LastAccessTime,LastWriteTime,Owner,Hash"
+$csv = "./csv/geoinfo_nas_folder_pwshell.csv"
+$campos > $csv
+. ./env.ps1
+
+$files = Get-ChildItem -Path $source -Recurse -Depth 10000
+
+ForEach ($file in $files) {
+    [array]$f = $file.FullName,
+         $file.Name,
+         $file.Extension,
+         $file.Length,
+         $file.CreationTime.ToString('yyyy-MM-ddTHH:mm:ss'),
+         $file.LastAccessTime.ToString('yyyy-MM-ddTHH:mm:ss'),
+         $file.LastWriteTime.ToString('yyyy-MM-ddTHH:mm:ss'),
+         (Get-Acl $file.FullName).Owner,
+         (Get-FileHash $file.FullName -Algorithm MD5).Hash
+         [string]$linha = $f -join ","
+         Add-Content -Path $csv -Value $linha
+         echo $file.FullName
+}
